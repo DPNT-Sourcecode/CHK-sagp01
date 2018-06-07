12080c47db4565e9603d16d364ff78594d1dfd3d
sbx_user1059 <sbx_user1059@ip-10-12-123-216.eu-west-2.compute.internal>
2018-06-07 12:42:59 +0000

    Thu Jun 07 12:42:59 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -3,7 +3,35 @@ package befaster.solutions.CHK;
 import befaster.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
-    public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+    public Integer checkout(String skus) 
+    {
+    	
+    	// this one doesn't make a lot of senses !!
+    	// we can return the value for A , B C etc.... but with a single parameter we can't specify the quantity.
+    	// unless we do something like pass in a JSON object.
+    	// to read that though we need a specification for the object.
+    	// this solution will just return the price but not taking into account the quality discount.
+    	
+    	if (skus.equals("A"))
+    	{
+    		return 50;
+    	}
+    	else if (skus.equals("B"))
+    	{
+    		return 30;
+    	}
+    	else if (skus.equals("A"))
+    	{
+    		return 20;
+    	}
+    	else if (skus.equals("A"))
+    	{
+    		return 15;
+    	}
+    	else
+    	{
+    		return -1;
+    	}
+    	
     }
 }
