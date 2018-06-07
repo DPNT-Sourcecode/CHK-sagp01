0857212e68df460cc3c56dea60cc46bcdffa5c09
sbx_user1059 <sbx_user1059@ip-10-12-123-216.eu-west-2.compute.internal>
2018-06-07 12:10:26 +0000

    Thu Jun 07 12:10:26 UTC 2018

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -4,8 +4,31 @@ import befaster.runner.SolutionNotImplementedException;
 
 public class SumSolution {
 
-    public int compute(int x, int y) {
-        throw new SolutionNotImplementedException();
+/**	SUM_R1
+	ROUND 1 - Add two numbers
+
+	Use this small challenge to understand how the system works.
+
+
+
+	For your convenience, a test framework has already been configured for this project.
+
+	In order to complete the round you need to implement the following method:
+	     sum(Integer, Integer) -> Integer
+
+	Where:
+	 - param[0] = a positive integer between 0-100
+	 - param[1] = a positive integer between 0-100
+	 - @return = an Integer representing the sum of the two numbers
+
+	**/
+	
+    public int compute(int x, int y) 
+    {
+    	
+    	/// this one returns an int not an Integer ..!!!
+    	// I'm assuming you meant int not Integer ...
+    	return x+y;
     }
 
 }
