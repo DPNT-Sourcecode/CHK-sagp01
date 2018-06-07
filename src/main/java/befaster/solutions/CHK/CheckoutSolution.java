57231f7ca3abb20329e5080e70a2b404f0c4a4ad
sbx_user1059 <sbx_user1059@ip-10-11-103-241.eu-west-2.compute.internal>
2018-06-07 13:38:40 +0000

    Thu Jun 07 13:38:40 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -6,32 +6,60 @@ public class CheckoutSolution {
     public Integer checkout(String skus) 
     {
     	
+    	String currentChar="";
+    	Integer ACount =0;
+    	Integer ADiscount=0;
+    	Integer BDiscount=0;
+    	Integer BCount =0;
+    	Integer CCount =0;
+    	Integer DCount =0;
+    	Integer total=0;
     	// this one doesn't make a lot of senses !!
     	// we can return the value for A , B C etc.... but with a single parameter we can't specify the quantity.
     	// unless we do something like pass in a JSON object.
     	// to read that though we need a specification for the object.
     	// this solution will just return the price but not taking into account the quality discount.
+    	for (int charNo=0; charNo<=skus.length();charNo++)
+    	{
+    		currentChar=skus.substring(charNo, charNo+1);
+    		if (currentChar.equals("A"))
+    		{
+    			ACount++;
+    			if (ACount>3)
+    			{
+    				ADiscount++;
+    				ACount=0;
+    			}
+    		}
+    		else if (currentChar.equals("B"))
+    		{
+    			BCount++;
+    			if (BCount>2)
+    			{
+    				BDiscount++;
+    				BCount=0;
+    			}
+    	   	}
+    		else if (currentChar.equals("C"))
+    		{
+    			CCount++;
+    	 	}
+    		else if (currentChar.equals("D"))
+    		{
+    			DCount++;
+    	    }
+    		else
+    		{
+    			return -1;
+    		}
+    	}
     	
-    	if (skus.equals("A"))
-    	{
-    		return 50;
-    	}
-    	else if (skus.equals("B"))
-    	{
-    		return 30;
-    	}
-    	else if (skus.equals("A"))
-    	{
-    		return 20;
-    	}
-    	else if (skus.equals("A"))
-    	{
-    		return 15;
-    	}
-    	else
-    	{
-    		return -1;
-    	}
+    	// calculat the totals 
+    	// logic assumes that all the A or B won't be in sequence but will still qualify fir a discount
+    	total = (ACount*50) + (ADiscount*130) + (BCount*30) + (BDiscount*45) + (CCount*20) + (DCount*15);
+    	
+    	return total;
+    	
     	
     }
 }
